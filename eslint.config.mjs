e9db4859ceb670f6c72727b11cd51dfa02e3e8cc
Morong Radek <morra@ton.cz>
2025-03-14 18:09:04 +0100

    initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+  recommendedConfig: js.configs.recommended,
+});
+
+const eslintConfig = [
+  ...compat.config({
+    extends: ["eslint:recommended", "next", "plugin:prettier/recommended"],
+  }),
+  {
+    ignores: [".next/*", ".prettierrc.js", "out/*"],
+  },
+];
+
+export default eslintConfig;
