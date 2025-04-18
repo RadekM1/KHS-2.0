3ee496f4b9d9e047bc54ac35ec0c13e9e205db68
Morong Radek <morra@ton.cz>
2025-04-18 15:20:26 +0200

    feat: SEO optimatization

--- src/components/index/logo.tsx
+++ src/components/index/logo.tsx
@@ -4,17 +4,17 @@ export default function Logo() {
   return (
     <div className="flex mt-16 text-center flex-col">
       <div className="flex-center flex w-full text-center filter transition duration-500 ease-in-out dark:brightness-75">
-      <Image
-        src='/launch.webp'
-        alt="Landing Image"
-        width={1700}
-        height={500}
-        priority
-        placeholder="blur"
-        blurDataURL='\blurr.webp'   
-        sizes="(max-width: 768px) 0px, 1200px" 
-        className="hidden md:block"
-      />
+        <Image
+          src="/launch.webp"
+          alt="Landing Image"
+          width={1700}
+          height={500}
+          priority
+          placeholder="blur"
+          blurDataURL="\blurr.webp"
+          sizes="(max-width: 768px) 0px, 1200px"
+          className="hidden md:block"
+        />
       </div>
     </div>
   );
