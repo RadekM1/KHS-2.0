d5fdd3201155327d153b9eaa17914cafacc5a6cf
Morong Radek <morra@ton.cz>
2025-05-31 17:28:35 +0200

    feat: google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import { Footer } from "../components/footer";
 import { Suspense } from "react";
 import { FooterNoParams } from "../components/footer-no-params";
 import { MenuNoParams } from "../components/menu/menu-no-params";
+import Script from "next/script";
 
 const roboto = Roboto({
   weight: "400",
@@ -25,6 +26,20 @@ export default function RootLayout({
       <body
         className={`${roboto.variable} mx-auto font-roboto w-full flex flex-col justify-center bg-gray-100 dark:bg-zinc-950 antialiased`}
       >
+        <Script
+          strategy="afterInteractive"
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-77X8WLQ2VB"
+        ></Script>
+        <Script strategy="afterInteractive" id="google-analytics">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-77X8WLQ2VB');
+            `}
+        </Script>
         <AppProviders>
           <Suspense fallback={<MenuNoParams />}>
             <Menu />
