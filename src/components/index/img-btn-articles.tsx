39b8e403a44e4dbf6f02c49188bcb45282d231dc
Morong Radek <morra@ton.cz>
2025-06-09 12:58:23 +0200

    fix: UI bugs

--- src/components/index/img-btn-articles.tsx
+++ src/components/index/img-btn-articles.tsx
@@ -8,7 +8,7 @@ export const ImgBtnArticles = (img: ImgArticleBtnType) => {
         className="object-cover brightness-[0.6] group-hover:brightness-[0.8] transform-gpu transition-all ease-in-out duration-300 h-full w-full"
         src={img.src}
       />
-      <p className="absolute  text-2xl font-bold text-nowrap top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2">
+      <p className="absolute  text-xl md:text-2xl font-bold text-nowrap top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2">
         {img.title}
       </p>
       <span className="absolute top-[80%] -translate-y-[80%] h-1 opacity-100 group-hover:opacity-0 transform-gpu transition-all ease-in-out duration-300 bg-white w-5 font-bold text-xl left-1/2 -translate-x-1/2"></span>
